9042f44b0ca99a49b977aeabdc65ab72095406ba
Indra M <indrambly@gmail.com>
2025-10-30 09:58:17 +0530

    Time: 1 ms (54.99%) | Memory: 18.8 MB (15.83%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.py
+++ 801-is-graph-bipartite/is-graph-bipartite.py
@@ -2,7 +2,7 @@ class Solution:
     def isBipartite(self, graph: List[List[int]]) -> bool:
         n=len(graph)
         color=[-1]*n
-        '''def dfs(node,col):
+        def dfs(node,col):
             color[node]=col
             for i in graph[node]:
                 if color[i]==-1:
@@ -15,18 +15,5 @@ class Solution:
             if color[i]==-1:
                 if not dfs(i,0):
                     return False
-        return True'''
-        from collections import deque
-        for s in range(n):
-            if color[s]==-1:
-                q=deque([s])
-                color[s]=0
-                while q:
-                    node=q.popleft()
-                    for i in graph[node]:
-                        if color[i]==-1:
-                            color[i]=1-color[node]
-                            q.append(i)
-                        elif color[i]==color[node]:
-                            return False
         return True
+        
