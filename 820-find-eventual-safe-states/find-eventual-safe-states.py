8d2cbd4b6b5e12d9517170781f3667bd433d9bab
Indra M <indrambly@gmail.com>
2025-10-31 12:37:25 +0530

    Time: 67 ms (25.57%) | Memory: 23.9 MB (35.73%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.py
+++ 820-find-eventual-safe-states/find-eventual-safe-states.py
@@ -1,6 +1,6 @@
 class Solution:
     def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
-        n=len(graph)
+        '''n=len(graph)
         path=[0]*n
         safe=[0]*n
         v=[0]*n
@@ -20,5 +20,25 @@ class Solution:
             if v[i]==0:
                 dfs(i)
         ans=[i for i in range(n) if safe[i]==1 ]
-        return ans
-        
+        return ans'''
+        from collections import defaultdict, deque
+        rev_graph=defaultdict(list)
+        n=len(graph)
+        indegree=[0]*n
+        q=deque()
+        for i in range(n):
+            for j in graph[i]:
+                rev_graph[j].append(i)
+                indegree[i]+=1
+        for i in range(n):
+            if indegree[i]==0:
+                q.append(i)
+        res=[]
+        while q:
+            node=q.popleft()
+            res.append(node)
+            for neig in rev_graph[node]:
+                indegree[neig]-=1
+                if indegree[neig]==0:
+                    q.append(neig)
+        return sorted(res)
