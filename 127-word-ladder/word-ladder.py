65d3462b44e18cc98b7489c0f1f421ae0c80917a
Indra M <indrambly@gmail.com>
2025-11-02 14:32:20 +0530

    Time: 79 ms (62.34%) | Memory: 26 MB (5.89%) - LeetSync

---
+++ 127-word-ladder/word-ladder.py
@@ -0,0 +1,25 @@
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        wordset=set(wordList)
+        from collections import defaultdict, deque
+        pattern_map=defaultdict(list)
+        for word in wordset:
+            for i in range(len(word)):
+                pattern=word[:i]+"*"+word[i+1:]
+                pattern_map[pattern].append(word)
+        q=deque()
+        q.append((beginWord,1))
+        visited=set()
+        while q:
+            word, steps = q.popleft()
+            
+            if word== endWord:
+                return steps
+            for i in range(len(word)):               
+                pattern=word[:i]+ "*" + word[i+1:]
+                for neigh in pattern_map[pattern]:
+                    if neigh not in visited:
+                        q.append((neigh,steps+1))
+                        visited.add(neigh)
+                pattern_map[pattern ]=[]
+        return 0
