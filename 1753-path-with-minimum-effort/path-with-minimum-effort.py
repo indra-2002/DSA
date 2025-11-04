e8f53f3053e0de10f695dc01d05d07c1bc97e811
Indra M <indrambly@gmail.com>
2025-11-04 10:00:00 +0530

    Time: 256 ms (94.41%) | Memory: 19.1 MB (91.42%) - LeetSync

---
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.py
@@ -0,0 +1,24 @@
+class Solution:
+    def minimumEffortPath(self, heights: List[List[int]]) -> int:
+        import heapq
+        rows, cols = len(heights), len(heights[0])
+        dis=[[float('inf')]*cols for _ in range(rows)]
+        dis[0][0]=0
+        minq=[]
+        heapq.heapify(minq)
+        minq.append((0,0,0))
+        while minq:
+            distance, row, col = heapq.heappop(minq)
+            if row== rows-1 and col == cols-1:
+                
+                return distance
+            if distance> dis[row][col]:
+                continue
+            for i,j in [(1,0),(-1,0),(0,1),(0,-1)]:
+                nr, nc= row+i, col + j
+                if 0<= nr < rows and 0<= nc < cols:
+                    max_distance=max(distance,abs(heights[nr][nc]-heights[row][col]))
+                    if dis[nr][nc] > max_distance:
+                        dis[nr][nc]= max_distance
+                        heapq.heappush(minq,(dis[nr][nc],nr , nc))
+        return 0
