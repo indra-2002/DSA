ff96cce8b44f532a9bd1e6cd89394057ff7d47d9
Indra M <indrambly@gmail.com>
2025-11-05 11:53:03 +0530

    Time: 25 ms (19.44%) | Memory: 25.9 MB (9.98%) - LeetSync

---
+++ 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.py
@@ -0,0 +1,31 @@
+class Solution:
+    def countPaths(self, n: int, roads: List[List[int]]) -> int:
+        from collections import defaultdict
+        import heapq
+        graph=defaultdict(list)
+        for i, j, k in roads:
+            graph[i].append([j, k ])
+            graph[j].append([i, k ])
+        ways=[0]*n
+        dis=[float('inf')]*n
+        dis[0]=0
+        q=[]
+        heapq.heapify(q)
+        q.append((0,0))
+        ways[0]=1
+        while q:
+            distance, node = heapq.heappop(q)
+            
+            if distance> dis[node]:
+                continue
+            for neigh , wiegh in graph[node]:
+                
+                if dis[neigh] > distance+ wiegh:
+                    dis[neigh]=distance + wiegh
+                    
+                    ways[neigh]=ways[node]
+                    heapq.heappush(q,(dis[neigh],neigh))
+                elif distance+ wiegh == dis[neigh]:
+                    
+                    ways[neigh]=(ways[neigh]+ways[node])
+        return ways[n-1]%((10**9)+7)
