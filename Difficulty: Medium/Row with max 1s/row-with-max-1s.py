9fcbac91910dd6edacdc84da42670bdc5b7b9f8b
Indra M <indrambly@gmail.com>
2025-12-04 12:57:15 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Row with max 1s/row-with-max-1s.py
@@ -0,0 +1,24 @@
+class Solution:
+    def rowWithMax1s(self, arr):
+        # code here
+        def count(arr):
+            l=0
+            h=len(arr)-1
+            ans=len(arr)
+            while l<=h:
+                mid=(l+h)//2
+                if arr[mid]==1:
+                    ans=mid
+                    h=mid-1
+                else:
+                    l=mid+1
+            return ans
+        
+        maxi=float('-inf')
+        index=0
+        for i in range(len(mat)):
+            c=len(mat[i])-count(mat[i]) 
+            if c > maxi:
+                maxi=c
+                index=i
+        return index
