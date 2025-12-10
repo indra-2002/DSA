e682ff64b409d548481dd1533ac609dfd0d2f7d8
Indra M <indrambly@gmail.com>
2025-12-10 13:53:26 +0530

    Time: 85 ms (47.09%) | Memory: 18.3 MB (33.88%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -1,27 +1,21 @@
 from collections import Counter
-class Solution(object):
-    def minWindow(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: str
-        """
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
         need=Counter(t)
         left=start=end=0
-        window=Counter()
         missing=len(t)
-        for right,char in enumerate(s):
-            
-            window[char]+=1
-            if need[char]>= window[char]:
-
+        word=Counter()
+        
+        for index,val in enumerate(s):
+            word[val]+=1
+            if word[val]<=need[val]:
                 missing-=1
             while missing==0:
-                if end == 0 or right+1 - left < end - start:
-                    start,end = left , right+1
-                window[s[left]]-=1
-                if need[s[left]]> window [s[left]]:
+                if end == 0 or index+1 - left < end-start:
+                    start,end=left,index+1
+                word[s[left]]-=1
+                if need[s[left]]>word[s[left]]:
                     missing+=1
                 left+=1
-                
+           
         return s[start:end]
