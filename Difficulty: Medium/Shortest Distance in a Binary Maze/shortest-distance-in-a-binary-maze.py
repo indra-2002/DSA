8b4465bbc7865fcbf89a1e85df34e55063e1ee23
Indra M <indrambly@gmail.com>
2026-01-02 15:09:06 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Shortest Distance in a Binary Maze/shortest-distance-in-a-binary-maze.py
+++ Difficulty: Medium/Shortest Distance in a Binary Maze/shortest-distance-in-a-binary-maze.py
@@ -6,25 +6,25 @@ class Solution:
     
     def shortestPath(self, grid: List[List[int]], source: List[int], destination: List[int]) -> int:
         # code here
-        n=len(grid)
-        m=len(grid[0])
+        row=len(grid)
+        col=len(grid[0])
         if source==destination:
             return 0
-        dis=[[float('inf')]*m for _ in range(n)]
+        dis=[[float("inf")]*col for _ in range(row)]
         dis[source[0]][source[1]]=0
         from collections import deque
         q=deque()
         q.append((0,source[0],source[1]))
         while q:
-            distance, row, col = q.popleft()
+            distance,r,c=q.popleft()
             for i,j in [(1,0),(-1,0),(0,1),(0,-1)]:
-                nr, nc= row+ i, col + j
-                if 0<=nr< n and  0<=nc<m and grid[nr][nc]==1:
-                    if dis[nr][nc]>distance+1:
-                        
+                nr,nc=r+i,c+j
+                if 0<=nr<row and 0<=nc<col and grid[nr][nc]==1:
+                    if distance+1< dis[nr][nc]:
                         dis[nr][nc]=distance+1
-                        if nr==destination[0] and nc== destination[1]:
+                        
+                        if [nr,nc]==destination:
                             return distance+1
-                    
-                        q.append((distance+1,nr,nc))
+                        q.append((dis[nr][nc],nr,nc))
         return -1
+                    
