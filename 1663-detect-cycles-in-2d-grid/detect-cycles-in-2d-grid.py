d3f19a825f5a0cdea4f7a0c5c865751a8d9f685f
Indra M <indrambly@gmail.com>
2026-01-10 12:44:15 +0530

    Time: 296 ms (78.99%) | Memory: 84.3 MB (71.31%) - LeetSync

--- 1663-detect-cycles-in-2d-grid/detect-cycles-in-2d-grid.py
+++ 1663-detect-cycles-in-2d-grid/detect-cycles-in-2d-grid.py
@@ -1,24 +1,25 @@
 class Solution:
     def containsCycle(self, grid: List[List[str]]) -> bool:
-        r=len(grid)
-        c=len(grid[0])
-        v=[[0]*c for _ in range(r)]
-        def dfs(row,col,pr,pc,ch):
-            if v[row][col]==1:
+        n=len(grid)
+        m=len(grid[0])
+        v=[[0]*m for _ in range(n)]
+        def dfs(r,c,pr,pc,ch):
+            if v[r][c]==1:
                 return True
-            v[row][col]=1
-            for i,j in [(1,0),(-1,0),(0,1),(0,-1)]:
-                nr=row+i
-                nc=col+j
-                if 0<=nr<r and 0<=nc<c and grid[nr][nc]==ch:
-                    if (nr,nc) == (pr,pc):
+            v[r][c]=1
+            for row,col in [(1,0),(-1,0),(0,1),(0,-1)]:
+                nr,nc=row+r,col+c
+                if 0<=nr<n and 0<=nc<m and grid[nr][nc]==ch:
+                    
+                    if (nr,nc)==(pr,pc):
                         continue
-                    if dfs(nr,nc,row,col,ch):
+                    if dfs(nr,nc,r,c,ch):
                         return True
-            return False
-        for i in range(r):
-            for j in range(c):
-                if not v[i][j]:
+        for i in range(n):
+            for j in range(m):
+                if v[i][j]==0:
                     if dfs(i,j,-1,-1,grid[i][j]):
                         return True
         return False
+
+            
