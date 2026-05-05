ca5776e14f201edb63cd9774168bc1daba378daf
Indra M <indrambly@gmail.com>
2026-05-05 18:11:05 +0530

    Time: 17 ms (49.26%) | Memory: 23.7 MB (6.32%) - LeetSync

---
+++ 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.py
@@ -0,0 +1,11 @@
+class Solution:
+    def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
+        import heapq
+        heap=[]
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                
+                heapq.heappush(heap,(-matrix[i][j]))
+                if len(heap)>k:
+                    heapq.heappop(heap)
+        return -(heap[0])
